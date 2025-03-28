260ca6f14c9cd4fb35764bc31fa672e033f4b043
MrThang <email.com>
2025-03-29 00:53:25 +0900

    Initial commit

---
+++ source/ur10_sim2real/ur10_sim2real/tasks/manager_based/ur10_sim2real/__init__.py
@@ -0,0 +1,23 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+import gymnasium as gym
+
+from . import agents
+
+##
+# Register Gym environments.
+##
+
+
+gym.register(
+    id="Template-Ur10-Sim2real-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.ur10_sim2real_env_cfg:Ur10Sim2realEnvCfg",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:PPORunnerCfg",
+    },
+)
